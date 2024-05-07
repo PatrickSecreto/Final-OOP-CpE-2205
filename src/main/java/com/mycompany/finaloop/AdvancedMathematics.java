3df218077bf994bf81fc9802f72e46d0125e6bb5
Patrick Secreto <22-07061@g.batstate-u.edu.ph>
2024-05-07 14:01:39 +0800

    Updated the functions in Advanced Mathematics

--- src/main/java/com/mycompany/finaloop/AdvancedMathematics.java
+++ src/main/java/com/mycompany/finaloop/AdvancedMathematics.java
@@ -8,30 +8,52 @@ public class AdvancedMathematics {
         Scanner scanner = new Scanner(System.in);
         int choice = 0;
         
-        while (choice != 4){
+        while (choice != 3){
             System.out.println("\nWhat lesson in Numerical Mathematics do you want to learn?\n"
-                    + "1. Add or Subtract Matrices\n"
-                    + "2. Multiply Matrices\n"
-                    + "3. Determinants of Matrices\n"
-                    + "4. Exit");
+                    + "1. Add, Subtract or Multiply Matrices\n"
+                    + "2. Determinants of Matrices\n"
+                    + "3. Exit");
 
             System.out.print("\nEnter your choice: ");
             choice = scanner.nextInt();
             
             switch (choice){
                 case 1:
-                    ChoiceAddingSubtractingMatrices();
+                    AddSubtractMultiplyMatrices asm = new AddSubtractMultiplyMatrices();
+                    
+                    asm.ChoiceAddSubtractMultiplyMatrices();
                     break;
                     
                 case 2:
-                    ChoicesMultiplyingMatrices();
-                    break;
-                    
-                case 3:
-                    ChoicesDeterminantsOfMatrices();
+                    int dchoice = 0;
+                    while (dchoice != 3){
+                        System.out.println("\nDeterminants: Calculate for?\n"
+                                    + "1. Information about Determinants\n"
+                                    + "2. Determinant Calculator\n"
+                                    + "3. Back");
+                        
+                        System.out.print("\nEnter your choice: ");
+                        dchoice = scanner.nextInt();
+                        
+                        switch(dchoice){
+                            case 1:
+                                System.out.println("\nThe determinant is a special number that can be calculated from \n"
+                                                 + "a matrix. The determinant helps us to find the inverse of a matrix, \n"
+                                                 + "telling us things about the matrix that are useful in systems of \n"
+                                                 + "linear equations, calculus and more.");
+                                break;
+                                
+                            case 2:
+                                DeterminantMatrices dm = new DeterminantMatrices();
+                                dm.ChoiceDeterminantMatrices();
+                                break;
+                            case 3:
+                                break;
+                        }
+                    }
                     break;    
                     
-                case 4:
+                case 3:
                     break;
                 
                 default:
@@ -39,20 +61,5 @@ public class AdvancedMathematics {
                     break;
             }
         }
-    }
-    
-    
-    public static void ChoiceAddingSubtractingMatrices(){
-        
-    }
-    
-    
-    public static void ChoicesMultiplyingMatrices(){
-        
-    }
-    
-    
-    public static void ChoicesDeterminantsOfMatrices(){
-        
     }
 }
